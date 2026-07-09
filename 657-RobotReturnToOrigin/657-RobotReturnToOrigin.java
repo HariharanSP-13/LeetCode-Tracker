2be74a63b4d65bac835dbd015516d58199c711a7
HariharanSP-13 <hariharansp1316@gmail.com>
2026-07-09 09:10:23 +0530

    Create 657-RobotReturnToOrigin

---
+++ 657-RobotReturnToOrigin/657-RobotReturnToOrigin.java
@@ -0,0 +1,14 @@
+// Last updated: 7/9/2026, 9:10:23 AM
+public class Solution {
+    public boolean judgeCircle(String moves) {
+        int x = 0;
+        int y = 0;
+        for (char ch : moves.toCharArray()) {
+            if (ch == 'U') y++;
+            else if (ch == 'D') y--;
+            else if (ch == 'R') x++;
+            else if (ch == 'L') x--;
+        }
+        return x == 0 && y == 0;
+    }
+}
