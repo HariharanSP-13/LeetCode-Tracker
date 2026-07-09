e7eb4990fb0f9e371200f0fc0136d9ee0355b521
HariharanSP-13 <hariharansp1316@gmail.com>
2026-07-09 09:11:25 +0530

    Create 35-SearchInsertPosition

---
+++ 35-SearchInsertPosition/35-SearchInsertPosition.java
@@ -0,0 +1,12 @@
+// Last updated: 7/9/2026, 9:11:26 AM
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        //test
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>=target){
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
