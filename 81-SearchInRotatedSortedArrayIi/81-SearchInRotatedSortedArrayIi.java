999da8dae2abca738453cfdbfa3ae0d091303b62
HariharanSP-13 <hariharansp1316@gmail.com>
2026-07-09 09:11:10 +0530

    Create 81-SearchInRotatedSortedArrayIi

---
+++ 81-SearchInRotatedSortedArrayIi/81-SearchInRotatedSortedArrayIi.java
@@ -0,0 +1,11 @@
+// Last updated: 7/9/2026, 9:11:11 AM
+class Solution {
+    //test
+    public boolean search(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target)
+                return true;
+        }
+        return false;
+    }
+}
