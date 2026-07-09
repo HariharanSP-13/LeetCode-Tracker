ac41fdf2d1d077f4073f78fdd85bb62ddc1602e2
HariharanSP-13 <hariharansp1316@gmail.com>
2026-07-09 09:11:24 +0530

    Create 50-PowxN

---
+++ 50-PowxN/50-PowxN.java
@@ -0,0 +1,8 @@
+// Last updated: 7/9/2026, 9:11:24 AM
+import java.util.*;
+class Solution {
+    public double myPow(double x, int n) {
+        double res=Math.pow(x,n);
+        return res;
+    }
+}
